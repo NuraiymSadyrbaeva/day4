db0f73b898eb5c019a7bc3c017456ac69a95f47d
Nuraiym Sadyrbaeva <120064238+NuraiymSadyrbaeva@users.noreply.github.com>
2023-12-06 21:18:39 +0100

    Add files via upload
    w3

---
+++ index.py
@@ -0,0 +1,83 @@
+#w3School Tutorial 
+alter = "alter"
+Vorname = "Nura"
+Nachname = "Sadyrbaeva"
+voller_name = Vorname + " " + Nachname
+Datum = " heute ist 5.12.2023"
+
+#print("Moin", "ich bin " + voller_name + Datum)
+x = y = z = "Orange"
+#And you can assign the same value to multiple variables in one line:
+#print(x)
+#print(y)
+#print(z)
+
+#Many Values to Multiple Variables
+#Python allows you to assign values to multiple variables in one line:
+x, y, z = "Orange", "Banana", "Cherry"
+#print(x)
+#print(y)
+#print(z)
+
+firstDay, secondDay, thirdDay = "Montag", "Dienstag", "Mittwoch"
+#print(firstDay)
+#print(secondDay)
+#print(thirdDay)
+
+myworkdays = (firstDay, secondDay, thirdDay)
+#print("My workdays:", myworkdays)
+
+#Output Variables
+x = "Python is awesome"
+#print(x)
+
+
+#In the print() function, you output multiple variables, separated by a comma:
+x = "Python"
+y = "is"
+z = "awesome"
+#print(x, y, z)
+
+#Global Variables
+#men dagy bir jolu karaschym kerek
+
+x = "awesome"
+
+def myfunc():
+  x = "fantastic"
+  print("Python is " + x)
+
+myfunc()
+
+#print("Python is " + x)
+
+global_var = 10
+
+def my_func():
+    print("Глобальная переменная внутри функции:", global_var)
+
+my_func()
+#print("Глобальная переменная вне функции:", global_var)
+#Variable Exercises
+carname = "Volvo"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
